fb5fe559c6d0eb9f7d999f73dd43bb45c80201b3
DoHongNhi <48651073+DoHongNhi@users.noreply.github.com>
2019-05-19 22:25:10 +0700

    Tạo file PMT, CTPMT

---
+++ PhieuMuonTraDAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLTV.DAO
+{
+    class PhieuMuonTraDAO
+    {
+        public string MaPhieuMuonTra { get; set; }
+        public DateTime NgayMuon { get; set; }
+        public DateTime NgayHenTra { get; set; }
+        public DateTime NgayTra { get; set; }
+        public string SoThe { get; set; }
+        public string MaNhanVien { get; set; }
+
+        public PhieuMuonTraDAO(string MaPhieuMuonTra, DateTime NgayMuon, DateTime NgayHenTra, DateTime NgayTra, string SoThe, string MaNhanVien)
+        {
+            this.MaPhieuMuonTra = MaPhieuMuonTra;
+            this.NgayMuon = NgayMuon;
+            this.NgayHenTra = NgayHenTra;
+            this.NgayTra = NgayTra;
+            this.SoThe = SoThe;
+            this.MaNhanVien = MaNhanVien;
+        }
+    }
+}
